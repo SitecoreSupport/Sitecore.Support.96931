068881d0d9de07406fad588ad874ed4a40342b45
DK\IRO <iro@sitecore.net>
2018-09-26 22:19:06 +0300

    added LanguageFallbackSwitcher

--- src/Sitecore.Support.96931/ContentSearch/LuceneProvider/LuceneDocumentBuilder.cs
+++ src/Sitecore.Support.96931/ContentSearch/LuceneProvider/LuceneDocumentBuilder.cs
@@ -15,6 +15,74 @@
         : base(indexable, context)
     {
     }
-    
+
+    protected override void AddComputedIndexFieldsInParallel()
+    {
+      ConcurrentQueue<Exception> exceptions = new ConcurrentQueue<Exception>();
+      var needEnterLanguageFallbackItemSwitcher = LanguageFallbackItemSwitcher.CurrentValue;
+      ParallelForeachProxy.ForEach<IComputedIndexField>((IEnumerable<IComputedIndexField>)Options.ComputedIndexFields, ParallelOptions, (Action<IComputedIndexField, ParallelLoopState>)delegate (IComputedIndexField computedIndexField, ParallelLoopState parallelLoopState)
+      {
+        object fieldValue;
+        try
+        {
+          using (new LanguageFallbackItemSwitcher(needEnterLanguageFallbackItemSwitcher))
+          {
+            using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+            {
+              fieldValue = computedIndexField.ComputeFieldValue(Indexable);
+            }
+          }
+        }
+        catch (Exception ex)
+        {
+          CrawlingLog.Log.Warn($"Could not compute value for ComputedIndexField: {computedIndexField.FieldName} for indexable: {Indexable.UniqueId}", ex);
+          if (Settings.StopOnCrawlFieldError())
+          {
+            exceptions.Enqueue(ex);
+            parallelLoopState.Stop();
+          }
+          return;
+        }
+        using (new LanguageFallbackItemSwitcher(needEnterLanguageFallbackItemSwitcher))
+        {
+          using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+          {
+            AddComputedIndexField(computedIndexField, fieldValue);
+          }
+        }
+      });
+      if (!exceptions.IsEmpty)
+      {
+        throw new AggregateException(exceptions);
+      }
+    }
+
+    protected override void AddComputedIndexFieldsInSequence()
+    {
+      foreach (IComputedIndexField computedIndexField in Options.ComputedIndexFields)
+      {
+        object fieldValue;
+        try
+        {
+          using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+          {
+            fieldValue = computedIndexField.ComputeFieldValue(Indexable);
+          }
+        }
+        catch (Exception exception)
+        {
+          CrawlingLog.Log.Warn($"Could not compute value for ComputedIndexField: {computedIndexField.FieldName} for indexable: {Indexable.UniqueId}", exception);
+          if (Settings.StopOnCrawlFieldError())
+          {
+            throw;
+          }
+          continue;
+        }
+        using (new LanguageFallbackFieldSwitcher(this.Index.EnableFieldLanguageFallback))
+        {
+          AddComputedIndexField(computedIndexField, fieldValue);
+        }
+      }
+    }
   }
 }
